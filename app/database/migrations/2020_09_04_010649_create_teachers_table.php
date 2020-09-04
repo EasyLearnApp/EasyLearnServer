918cc83eb9997594a6416832f9820461125d3385
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-03 23:06:21 -0300

    Teacher Migration

---
+++ app/database/migrations/2020_09_04_010649_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('name', 32)->nullable(false)->unique();
+            $table->string('email', 32)->nullable(false)->unique();
+            $table->string('address', 32)->nullable(false)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
