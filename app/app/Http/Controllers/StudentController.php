eb9d031c7920c792d8e3e2c4ffb1d61208484601
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-05 17:29:26 -0300

    Updated StudentController

---
+++ app/app/Http/Controllers/StudentController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(\App\Student::all(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student = \App\Student::create($request->all());
+
+        return response()->json($student, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(\App\Student::find($id), 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(\App\Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, \App\Student $student)
+    {
+        foreach ((new \App\Student())->fillable as $property) {
+            $student->{$property} = $request->{$property} ?? $student->{$property};
+        }
+
+        $status = $student->save();
+
+        return response()->json($student, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(\App\Student $student)
+    {
+        $status = $student->delete();
+
+        return \Illuminate\Support\Facades\Response::make([], 200);
+    }
+}
