ec0b1281cff25af45558ae6fc022481549c68c66
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-05 17:29:16 -0300

    Updated GradeController

---
+++ app/app/Http/Controllers/GradeController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GradeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(\App\Grade::all(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $grade = \App\Grade::create($request->all());
+
+        return response()->json($grade, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(\App\Grade::find($id), 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(\App\Grade $grade)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, \App\Grade $grade)
+    {
+        foreach ((new \App\Grade())->fillable as $property) {
+            $grade->{$property} = $request->{$property} ?? $grade->{$property};
+        }
+
+        $status = $grade->save();
+
+        return response()->json($grade, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Grade  $grade
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(\App\Grade $grade)
+    {
+        $status = $grade->delete();
+
+        return response()->json([], 200);
+    }
+}
